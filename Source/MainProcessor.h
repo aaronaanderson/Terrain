d747c740f6adeed35665a6ba0a3ca72f8f5658aa
aaron_anderson <aaron.arthur.anderson@gmail.com>
2024-05-13 11:18:50 -0400

    Initial Boilerplate

---
+++ Source/MainProcessor.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <juce_audio_processors/juce_audio_processors.h>
+
+//==============================================================================
+class MainProcessor  : public juce::AudioProcessor, 
+                       private juce::ValueTree::Listener
+{
+public:
+    //==============================================================================
+    MainProcessor();
+    ~MainProcessor() override;
+
+    //==============================================================================
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+    using AudioProcessor::processBlock;
+
+    //==============================================================================
+    juce::AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    //==============================================================================
+    const juce::String getName() const override;
+
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override;
+    double getTailLengthSeconds() const override;
+
+    //==============================================================================
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int index) override;
+    const juce::String getProgramName (int index) override;
+    void changeProgramName (int index, const juce::String& newName) override;
+
+    //==============================================================================
+    void getStateInformation (juce::MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+
+private:
+    juce::ValueTree state;
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainProcessor)
+};
