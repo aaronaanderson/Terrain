8498545c97bf234c0c3214e0bcf8408e31e88c44
aaron_anderson <aaron.arthur.anderson@gmail.com>
2024-05-20 16:56:24 -0400

    extract ParameterSlider

---
+++ Source/Interface/ParameterSlider.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <juce_gui_basics/juce_gui_basics.h>
+#include "GlobalTimer.h"
+#include "../Parameters.h"
+
+namespace ti
+{
+struct ParameterSlider : public juce::Component,
+                         private GlobalTimer::Listener,
+                         private juce::AudioProcessorParameter::Listener
+{
+    ParameterSlider (juce::AudioProcessorParameter* p, GlobalTimer& gt, juce::String labelText, juce::Range<double> range)
+      : parameter (p)
+    {
+        label.setText (labelText, juce::dontSendNotification);
+        slider.setRange (range, 0.0);
+        slider.setTextBoxStyle (juce::Slider::TextEntryBoxPosition::NoTextBox, true, 20, 20);
+        addAndMakeVisible (label);
+        addAndMakeVisible (slider);
+        parameter->addListener (this);
+        gt.addListener (*this);
+    }
+    ~ParameterSlider()
+    {
+        parameter->removeListener (this);
+    }
+    void resized() override 
+    {
+        auto b = getLocalBounds();
+        if (label.getText().length() > 3)
+            label.setBounds (b.removeFromTop (20));
+        else
+            label.setBounds (b.removeFromLeft (20));
+        
+        slider.setBounds (b);
+    }
+
+    void setValue (double v) { slider.setValue (v, juce::NotificationType::dontSendNotification); }
+    juce::Slider& getSlider() { return slider; }
+private:
+    juce::AudioProcessorParameter* parameter;
+    juce::Label label;
+    juce::Slider slider;
+    bool needsRepainted = true;
+    void onTimerCallback() override 
+    { 
+        if (needsRepainted)
+        {
+            auto rangedParam = dynamic_cast<juce::RangedAudioParameter*> (parameter);
+            jassert (rangedParam != nullptr);
+            auto rangedValue = rangedParam->convertFrom0to1 (parameter->getValue());
+            slider.setValue (static_cast<double> (rangedValue), juce::dontSendNotification);
+            repaint(); 
+            needsRepainted = false;
+        }
+    }
+    void parameterValueChanged (int parameterIndex, float newValue) override 
+    {
+        juce::ignoreUnused (parameterIndex, newValue);
+        needsRepainted = true;
+    }
+    void parameterGestureChanged (int parameterIndex, bool gestureIsStarting) override { juce::ignoreUnused (parameterIndex, gestureIsStarting); }
+};
+}
--- Source/Interface/TrajectoryPanel.h
+++ Source/Interface/TrajectoryPanel.h
@@ -5,6 +5,7 @@
 #include "../Identifiers.h"
 #include "GlobalTimer.h"
 #include "../Parameters.h"
+#include "ParameterSlider.h"
 namespace ti
 {
 class TrajectorySelector : public juce::Component,
@@ -100,62 +101,7 @@ private:
     }
     void parameterGestureChanged (int parameterIndex, bool gestureIsStarting) override { juce::ignoreUnused (parameterIndex, gestureIsStarting); }          
 };
-struct ParameterSlider : public juce::Component,
-                         private GlobalTimer::Listener,
-                         private juce::AudioProcessorParameter::Listener
-{
-    ParameterSlider (juce::AudioProcessorParameter* p, GlobalTimer& gt, juce::String labelText, juce::Range<double> range)
-      : parameter (p)
-    {
-        label.setText (labelText, juce::dontSendNotification);
-        slider.setRange (range, 0.0);
-        slider.setTextBoxStyle (juce::Slider::TextEntryBoxPosition::NoTextBox, true, 20, 20);
-        addAndMakeVisible (label);
-        addAndMakeVisible (slider);
-        parameter->addListener (this);
-        gt.addListener (*this);
-    }
-    ~ParameterSlider()
-    {
-        parameter->removeListener (this);
-    }
-    void resized() override 
-    {
-        auto b = getLocalBounds();
-        if (label.getText().length() > 3)
-            label.setBounds (b.removeFromTop (20));
-        else
-            label.setBounds (b.removeFromLeft (20));
-        
-        slider.setBounds (b);
-    }
 
-    void setValue (double v) { slider.setValue (v, juce::NotificationType::dontSendNotification); }
-    juce::Slider& getSlider() { return slider; }
-private:
-    juce::AudioProcessorParameter* parameter;
-    juce::Label label;
-    juce::Slider slider;
-    bool needsRepainted = true;
-    void onTimerCallback() override 
-    { 
-        if (needsRepainted)
-        {
-            auto rangedParam = dynamic_cast<juce::RangedAudioParameter*> (parameter);
-            jassert (rangedParam != nullptr);
-            auto rangedValue = rangedParam->convertFrom0to1 (parameter->getValue());
-            slider.setValue (static_cast<double> (rangedValue), juce::dontSendNotification);
-            repaint(); 
-            needsRepainted = false;
-        }
-    }
-    void parameterValueChanged (int parameterIndex, float newValue) override 
-    {
-        juce::ignoreUnused (parameterIndex, newValue);
-        needsRepainted = true;
-    }
-    void parameterGestureChanged (int parameterIndex, bool gestureIsStarting) override { juce::ignoreUnused (parameterIndex, gestureIsStarting); }
-};
 static juce::ValueTree getCurrentTrajectoryBranch (juce::ValueTree trajectoriesBranch)
 {
     jassert (trajectoriesBranch.getType() == id::TRAJECTORIES);
