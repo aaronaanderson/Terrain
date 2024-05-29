ab9628cf1bed423d34c75e2a27a88e1a5834255d
Aaron Anderson <aaronanderson@Aarons-MacBook-Air.local>
2024-05-29 00:24:49 -0400

    add glm submod

---
+++ glm_module/glm.cpp
@@ -0,0 +1,9 @@
+//safety check, make sure nothing else relies on this first
+#ifdef FINAL_MIX_COMMON_H_INCLUDED
+ #error "Incorrect use of glm.cpp"
+#endif
+
+#include "glm.hpp"
+
+#include "glm/glm/glm.hpp"
+#include "glm/glm/ext.hpp"
---
+++ glm_module/glm.hpp
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ This is a simple module to include glm as a separate compilation unit to avoid 
+ issues on MacOS
+
+
+ BEGIN_JUCE_MODULE_DECLARATION
+
+  ID:                 glm
+  vendor:             Aaron
+  version:            6.0.7
+  name:               JUCE glm
+  description:        GLM as juce module
+  website:            
+  license:            
+  minimumCppStandard: 14
+
+  dependencies: 
+
+ END_JUCE_MODULE_DECLARATION
+
+*/
+
+#pragma once
+#define GLM_INCLUDED
