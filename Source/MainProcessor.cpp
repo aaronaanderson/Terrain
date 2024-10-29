0d8564bca92f7981340364f1687aaf5cfb26a8aa
aaron_anderson <aaron.arthur.anderson@gmail.com>
2024-10-29 13:34:19 -0400

    Clean Default Tree

--- Source/MainProcessor.cpp
+++ Source/MainProcessor.cpp
@@ -147,10 +147,13 @@ void MainProcessor::setStateInformation (const void* data, int sizeInBytes)
     std::unique_ptr<juce::XmlElement> xmlState (getXmlFromBinary (data, sizeInBytes));
 
     if (xmlState.get() != nullptr)
+    {
         if (xmlState->hasTagName (valueTreeState.state.getType()))
+        {
             valueTreeState.replaceState (juce::ValueTree::fromXml (*xmlState));
-
-    presetManager = std::make_unique<PresetManager> (this, valueTreeState.state);
+            presetManager = std::make_unique<PresetManager> (this, valueTreeState.state);
+        }
+    }
 }
 //==============================================================================
 juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter() { return new MainProcessor(); }
