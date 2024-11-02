b9e01cf09592a4a1de52da3e53ddab5e4f2a4e4a
aaron_anderson <aaron.arthur.anderson@gmail.com>
2024-11-02 19:55:36 -0400

    Detangle Standard Voice

---
+++ Source/DSP/StandardVoice.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <juce_audio_basics/juce_audio_basics.h>
+#include <juce_data_structures/juce_data_structures.h>
+#include <MTS-ESP/Client/libMTSClient.h>
+#include "VoiceInterface.h"
+#include "Trajectory.h"
+#include "Terrain.h"
+
+namespace tp
+{
+class StandardVoice : public VoiceInterface, 
+                      public juce::SynthesiserVoice
+{
+public:
+    StandardVoice (Terrain& t, 
+                   Parameters& p, 
+                   juce::ValueTree settingsBranch, 
+                   MTSClient& mtsc)
+      : trajectory (t, p, settingsBranch, mtsc)
+    {
+
+    }
+    // Voice Interface ===================================================
+    const float* getRawData() override { return trajectory.getRawData(); }
+    void prepareToPlay (double newRate, int blockSize) override 
+    {
+        trajectory.prepareToPlay (newRate, blockSize);
+    }
+    void setState (juce::ValueTree settingsBranch) override
+    {
+        trajectory.setState (settingsBranch);
+    }
+    // Synthesiser Voice ==================================================
+    bool canPlaySound (juce::SynthesiserSound* s) override { return dynamic_cast<DummySound*>(s) != nullptr; }
+    void startNote (int midiNoteNumber,
+                    float velocity,
+                    juce::SynthesiserSound* /*sound*/,
+                    int currentPitchWheelPosition) override 
+    {   
+        trajectory.startNote (midiNoteNumber, velocity, currentPitchWheelPosition);
+    }
+    void stopNote (float /*velocity*/, bool allowTailOff) override 
+    { 
+        if (!allowTailOff) clearCurrentNote(); 
+        trajectory.stopNote();
+    }
+    void pitchWheelMoved (int newPitchWheelValue) override 
+    { 
+        trajectory.pitchWheelMoved (newPitchWheelValue);
+    }
+    void controllerMoved (int controllerNumber, int newControllerValue) override 
+    { 
+        juce::ignoreUnused (controllerNumber, newControllerValue); 
+        trajectory.controllerMoved();
+    }
+    void renderNextBlock (juce::AudioBuffer<double>& ob, int ss, int nums) override 
+    {  
+        trajectory.renderNextBlock (ob, ss, nums);
+        if (trajectory.shouldClear()) clearCurrentNote();
+    }
+    void renderNextBlock (juce::AudioBuffer<float>& outputBuffer, 
+                          int startSample, int numSamples) override 
+    {
+        trajectory.renderNextBlock (outputBuffer, startSample, numSamples);
+    }
+    void setCurrentPlaybackSampleRate (double newRate) override 
+    {
+        trajectory.setCurrentPlaybackSampleRate (newRate);
+    }
+private:
+    Trajectory trajectory;
+};
+}
