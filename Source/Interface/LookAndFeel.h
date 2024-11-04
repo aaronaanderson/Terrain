8a4d9a02f39e146c9aa07b364bc435c9f453cecb
aaron_anderson <aaron.arthur.anderson@gmail.com>
2024-11-04 11:43:44 -0500

    Adjust range slider lookandfeel

--- Source/Interface/LookAndFeel.h
+++ Source/Interface/LookAndFeel.h
@@ -37,11 +37,31 @@ public:
                                                                        BinaryData::Paulle1V_ttfSize);
         return typeface;
     }
+    void drawPointer (juce::Graphics& g, const float x, const float y, const float diameter,
+                      const juce::Colour& colour, const int direction) noexcept
+    {
+        // juce::ignoreUnused (g, x, y, diameter, colour, direction);
+        // g.setColour (colour);
+        // g.fillRect (x, y, diameter, diameter);
+        juce::Path p;
+        p.startNewSubPath (x + diameter * 0.5f, y);
+        p.lineTo (x + diameter, y + diameter * 0.6f);
+        p.lineTo (x + diameter, y + diameter);
+        p.lineTo (x, y + diameter);
+        p.lineTo (x, y + diameter * 0.6f);
+        p.closeSubPath();
+    
+        p.applyTransform (juce::AffineTransform::rotation ((float) direction * juce::MathConstants<float>::halfPi,
+                                                     x + diameter * 0.5f, y + diameter * 0.5f));
+        g.setColour (colour);
+        g.fillPath (p);
+    }
     void drawLinearSlider (juce::Graphics& g, int x, int y, int width, int height,
                                            float sliderPos,
                                            float minSliderPos,
                                            float maxSliderPos,
-                                           const juce::Slider::SliderStyle style, juce::Slider& slider) override
+                                           const juce::Slider::SliderStyle style, 
+                                           juce::Slider& slider) override
     {
         if (slider.isBar())
         {
@@ -114,13 +134,13 @@ public:
     
                 if (slider.isHorizontal())
                 {
-                    drawPointer (g, minSliderPos - sr,
-                                 juce::jmax (0.0f, (float) y + (float) height * 0.5f - trackWidth * 2.0f),
-                                 trackWidth * 2.0f, pointerColour, 2);
+                    drawPointer (g, minSliderPos - (trackWidth),
+                                 juce::jmax (0.0f, (float) y + (float) height * 0.5f - trackWidth),
+                                 trackWidth, pointerColour, 2);
     
-                    drawPointer (g, maxSliderPos - trackWidth,
-                                 juce::jmin ((float) (y + height) - trackWidth * 2.0f, (float) y + (float) height * 0.5f),
-                                 trackWidth * 2.0f, pointerColour, 4);
+                    drawPointer (g, maxSliderPos, //- trackWidth,
+                                 juce::jmin ((float) (y + height) - trackWidth, (float) y + (float) height * 0.5f),
+                                 trackWidth, pointerColour, 4);
                 }
                 else
                 {
@@ -137,7 +157,6 @@ public:
                 drawLinearSliderOutline (g, x, y, width, height, style, slider);
         }
     }
-    
     void drawRotarySlider (juce::Graphics& g, int x, int y, int width, int height, float sliderPos,
                            const float rotaryStartAngle, const float rotaryEndAngle, juce::Slider& slider) override
     {
@@ -198,7 +217,6 @@ public:
                                 true);
         g.strokePath (thumbArc, juce::PathStrokeType (lineW, juce::PathStrokeType::mitered, juce::PathStrokeType::butt));
     }   
-    
     void drawTickBox (juce::Graphics& g, juce::Component& component,
                       float x, float y, float w, float h,
                       const bool ticked,
@@ -226,86 +244,85 @@ public:
         return button.getTabbedButtonBar().getWidth() / 
                button.getTabbedButtonBar().getNumTabs();
     }
-
-void drawTabButton (juce::TabBarButton& button, juce::Graphics& g, bool isMouseOver, bool isMouseDown) override
-{
-    const juce::Rectangle<int> activeArea (button.getActiveArea());
-
-    const juce::TabbedButtonBar::Orientation o = button.getTabbedButtonBar().getOrientation();
-
-    const juce::Colour bkg (button.getTabBackgroundColour());
-
-    if (button.getToggleState())
+    void drawTabButton (juce::TabBarButton& button, juce::Graphics& g, bool isMouseOver, bool isMouseDown) override
     {
-        g.setColour (bkg);
-    }
-    else
-    {
-        juce::Point<int> p1, p2;
-
+        const juce::Rectangle<int> activeArea (button.getActiveArea());
+    
+        const juce::TabbedButtonBar::Orientation o = button.getTabbedButtonBar().getOrientation();
+    
+        const juce::Colour bkg (button.getTabBackgroundColour());
+    
+        if (button.getToggleState())
+        {
+            g.setColour (bkg);
+        }
+        else
+        {
+            juce::Point<int> p1, p2;
+    
+            switch (o)
+            {
+                case juce::TabbedButtonBar::TabsAtBottom:   p1 = activeArea.getBottomLeft(); p2 = activeArea.getTopLeft();    break;
+                case juce::TabbedButtonBar::TabsAtTop:      p1 = activeArea.getTopLeft();    p2 = activeArea.getBottomLeft(); break;
+                case juce::TabbedButtonBar::TabsAtRight:    p1 = activeArea.getTopRight();   p2 = activeArea.getTopLeft();    break;
+                case juce::TabbedButtonBar::TabsAtLeft:     p1 = activeArea.getTopLeft();    p2 = activeArea.getTopRight();   break;
+                default:                              jassertfalse; break;
+            }
+    
+            // g.setGradientFill (juce::ColourGradient (bkg.brighter (0.2f), p1.toFloat(),
+            //                                    bkg.darker (0.1f),   p2.toFloat(), false));
+            g.setColour (getBaseColour());
+        }
+    
+        g.fillRect (activeArea);
+    
+        g.setColour (button.findColour (juce::TabbedButtonBar::tabOutlineColourId));
+    
+        juce::Rectangle<int> r (activeArea);
+    
+        if (o != juce::TabbedButtonBar::TabsAtBottom)   g.fillRect (r.removeFromTop (1));
+        if (o != juce::TabbedButtonBar::TabsAtTop)      g.fillRect (r.removeFromBottom (1));
+        if (o != juce::TabbedButtonBar::TabsAtRight)    g.fillRect (r.removeFromLeft (1));
+        if (o != juce::TabbedButtonBar::TabsAtLeft)     g.fillRect (r.removeFromRight (1));
+    
+        const float alpha = button.isEnabled() ? ((isMouseOver || isMouseDown) ? 1.0f : 0.8f) : 0.3f;
+    
+        juce::Colour col (bkg.contrasting().withMultipliedAlpha (alpha));
+    
+        if (juce::TabbedButtonBar* bar = button.findParentComponentOfClass<juce::TabbedButtonBar>())
+        {
+            juce::TabbedButtonBar::ColourIds colID = button.isFrontTab() ? juce::TabbedButtonBar::frontTextColourId
+                                                                         : juce::TabbedButtonBar::tabTextColourId;
+    
+            if (bar->isColourSpecified (colID))
+                col = bar->findColour (colID);
+            else if (isColourSpecified (colID))
+                col = findColour (colID);
+        }
+    
+        const juce::Rectangle<float> area (button.getTextArea().toFloat());
+    
+        float length = area.getWidth();
+        float depth  = area.getHeight();
+    
+        if (button.getTabbedButtonBar().isVertical())
+            std::swap (length, depth);
+    
+        juce::TextLayout textLayout;
+        createTabTextLayout (button, length, depth, col, textLayout);
+    
+        juce::AffineTransform t;
+    
         switch (o)
         {
-            case juce::TabbedButtonBar::TabsAtBottom:   p1 = activeArea.getBottomLeft(); p2 = activeArea.getTopLeft();    break;
-            case juce::TabbedButtonBar::TabsAtTop:      p1 = activeArea.getTopLeft();    p2 = activeArea.getBottomLeft(); break;
-            case juce::TabbedButtonBar::TabsAtRight:    p1 = activeArea.getTopRight();   p2 = activeArea.getTopLeft();    break;
-            case juce::TabbedButtonBar::TabsAtLeft:     p1 = activeArea.getTopLeft();    p2 = activeArea.getTopRight();   break;
-            default:                              jassertfalse; break;
+            case juce::TabbedButtonBar::TabsAtLeft:   t = t.rotated (juce::MathConstants<float>::pi * -0.5f).translated (area.getX(), area.getBottom()); break;
+            case juce::TabbedButtonBar::TabsAtRight:  t = t.rotated (juce::MathConstants<float>::pi *  0.5f).translated (area.getRight(), area.getY()); break;
+            case juce::TabbedButtonBar::TabsAtTop:
+            case juce::TabbedButtonBar::TabsAtBottom: t = t.translated (area.getX(), area.getY()); break;
+            default:                            jassertfalse; break;
         }
-
-        // g.setGradientFill (juce::ColourGradient (bkg.brighter (0.2f), p1.toFloat(),
-        //                                    bkg.darker (0.1f),   p2.toFloat(), false));
-        g.setColour (getBaseColour());
+    
+        g.addTransform (t);
+        textLayout.draw (g, juce::Rectangle<float> (length, depth));
     }
-
-    g.fillRect (activeArea);
-
-    g.setColour (button.findColour (juce::TabbedButtonBar::tabOutlineColourId));
-
-    juce::Rectangle<int> r (activeArea);
-
-    if (o != juce::TabbedButtonBar::TabsAtBottom)   g.fillRect (r.removeFromTop (1));
-    if (o != juce::TabbedButtonBar::TabsAtTop)      g.fillRect (r.removeFromBottom (1));
-    if (o != juce::TabbedButtonBar::TabsAtRight)    g.fillRect (r.removeFromLeft (1));
-    if (o != juce::TabbedButtonBar::TabsAtLeft)     g.fillRect (r.removeFromRight (1));
-
-    const float alpha = button.isEnabled() ? ((isMouseOver || isMouseDown) ? 1.0f : 0.8f) : 0.3f;
-
-    juce::Colour col (bkg.contrasting().withMultipliedAlpha (alpha));
-
-    if (juce::TabbedButtonBar* bar = button.findParentComponentOfClass<juce::TabbedButtonBar>())
-    {
-        juce::TabbedButtonBar::ColourIds colID = button.isFrontTab() ? juce::TabbedButtonBar::frontTextColourId
-                                                                     : juce::TabbedButtonBar::tabTextColourId;
-
-        if (bar->isColourSpecified (colID))
-            col = bar->findColour (colID);
-        else if (isColourSpecified (colID))
-            col = findColour (colID);
-    }
-
-    const juce::Rectangle<float> area (button.getTextArea().toFloat());
-
-    float length = area.getWidth();
-    float depth  = area.getHeight();
-
-    if (button.getTabbedButtonBar().isVertical())
-        std::swap (length, depth);
-
-    juce::TextLayout textLayout;
-    createTabTextLayout (button, length, depth, col, textLayout);
-
-    juce::AffineTransform t;
-
-    switch (o)
-    {
-        case juce::TabbedButtonBar::TabsAtLeft:   t = t.rotated (juce::MathConstants<float>::pi * -0.5f).translated (area.getX(), area.getBottom()); break;
-        case juce::TabbedButtonBar::TabsAtRight:  t = t.rotated (juce::MathConstants<float>::pi *  0.5f).translated (area.getRight(), area.getY()); break;
-        case juce::TabbedButtonBar::TabsAtTop:
-        case juce::TabbedButtonBar::TabsAtBottom: t = t.translated (area.getX(), area.getY()); break;
-        default:                            jassertfalse; break;
-    }
-
-    g.addTransform (t);
-    textLayout.draw (g, juce::Rectangle<float> (length, depth));
-}
 };
